a1bf5e2d20359e530b8e1abd15090b7557a65803
Jong <yomybaby@yomybaby.com>
2018-10-30 20:35:17 +0800

    0.57 버전에서 동작하도록 수정

--- WeatherProejct.js
+++ WeatherProejct.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
-import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+import { StyleSheet, Text, View, TextInput, ImageBackground, StatusBar } from "react-native";
 
 import Forecast from "./Forecast";
 import OpenWeatherMap from "./open_weather_map";
@@ -30,29 +30,30 @@ class WeatherProject extends Component {
       );
     }
     return (
-      <View style={styles.container}>
-        <Image
-          source={require("./flowers.png")}
-          resizeMode="cover"
-          style={styles.backdrop}
-        >
-          <View style={styles.overlay}>
-            <View style={styles.row}>
-              <Text style={styles.mainText}>
-                Current weather for
-              </Text>
-              <View style={styles.zipContainer}>
-                <TextInput
-                  style={[styles.zipCode, styles.mainText]}
-                  onSubmitEditing={this._handleTextChange}
-                  underlineColorAndroid="transparent"
-                />
-              </View>
-            </View>
+      <Fragment>
+        <StatusBar
+          translucent
+          backgroundColor="rgba(255, 0, 0, 0.20)"
+          animated
+        />
+        <View style={styles.container}>
+          <ImageBackground
+            source={require("./flowers.png")}
+            resizeMode="cover"
+            style={[styles.backdrop, { alignItems: 'center'}]}
+          >
+            <Text style={styles.mainText}>
+              Current weather for
+            </Text>
+            <TextInput
+              style={[styles.zipCode, styles.mainText]}
+              onSubmitEditing={this._handleTextChange}
+              underlineColorAndroid="transparent"
+            />
             {content}
-          </View>
-        </Image>
-      </View>
+          </ImageBackground>
+        </View>
+      </Fragment>
     );
   }
 }
@@ -60,8 +61,8 @@ class WeatherProject extends Component {
 const baseFontSize = 16;
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center", paddingTop: 30 },
-  backdrop: { flex: 1, flexDirection: "column" },
+  container: { flex: 1, alignItems: "center"},
+  backdrop: { flex: 1, width:'100%', flexDirection: "column" , paddingTop: 30 },
   overlay: {
     paddingTop: 5,
     backgroundColor: "#000000",
@@ -82,7 +83,7 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginTop: 3
   },
-  zipCode: { flex: 1, flexBasis: 1, width: 50, height: baseFontSize },
+  zipCode: { width: 50, height: baseFontSize*2 ,  borderWidth: 2},
   mainText: { fontSize: baseFontSize, color: "#FFFFFF" }
 });
 
