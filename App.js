7aa1cf68b301e0b253a64444f1b46c2f52616535
Jong <yomybaby@yomybaby.com>
2018-11-02 17:06:14 +0800

    chapter 4 bestseller 예제 파일 복사 https://github.com/bonniee/learning-react-native/tree/master/src/bestsellers

--- App.js
+++ App.js
@@ -8,6 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import BookList from './bestsellers/BookList';
+import BookSectionList from './bestsellers/BookSectionList';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,11 +22,7 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <BookList/>
     );
   }
 }
