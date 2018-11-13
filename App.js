5c424427da23068535a0ad3d0dbd97ec883f528d
Jong <yomybaby@yomybaby.com>
2018-11-13 20:00:16 +0800

    NativeModules 로그 추가

--- App.js
+++ App.js
@@ -7,7 +7,9 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, NativeModules} from 'react-native';
+
+console.log(NativeModules);
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
